53bdf93a0402a0fc50e55c0022cc0508799d3a9b
Gydermes <gydermes23@gmail.com>
2022-09-15 01:15:14 +0300

    commit

--- news/views.py
+++ news/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from .models import Artiles
 # Create your views here.
 
 
 def news_home(request):
-    return render(request, 'news/news_home.html')
+    news = Artiles.objects.all()
+    return render(request, 'news/news_home.html', {"news": news})
+
+
+def create(request):
+    return render(request, 'news/create.html')
