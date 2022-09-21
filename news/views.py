f41408c2196f2353f7fd1c6524c6cda15e10657a
Gydermes <gydermes23@gmail.com>
2022-09-21 03:18:43 +0300

    commit

--- news/views.py
+++ news/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render, redirect
 from .models import Artiles
 from .forms import ActiclesForm
-# Create your views here.
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def news_home(request):
-    news = Artiles.objects.all()
+    news = Artiles.objects.order_by('date')
     return render(request, 'news/news_home.html', {"news": news})
+
+class NewsDetailView(DetailView):
+    model = Artiles
+    template_name = 'news/details_view.html'
+    context_object_name ='article'
+
+
+class NewsUpdateViews(UpdateView):
+    model = Artiles
+    template_name = 'news/create.html'
+
+    form_class = ActiclesForm
+
+
+class NewsDeleteViews(DeleteView):
+    model = Artiles
+    success_url = '/news/'
+    template_name = 'news/news_delete.html'
+
 
 
 def create(request):
